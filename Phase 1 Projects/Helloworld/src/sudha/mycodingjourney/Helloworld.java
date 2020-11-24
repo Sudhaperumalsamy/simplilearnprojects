c1949874852d055fa02326c46e8f3b9a68e48de7
Sudhaperumalsamy <sudha.perumalsamy@yahoo.com>
2020-11-24 13:59:00 +0530

    sixth commit

--- Phase 1 Projects/Helloworld/src/sudha/mycodingjourney/Helloworld.java
+++ Phase 1 Projects/Helloworld/src/sudha/mycodingjourney/Helloworld.java
@@ -2,11 +2,10 @@ package sudha.mycodingjourney;
 
 public class Helloworld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World,my java");
 	
-		
-
 	}
 
 }

22443cbbd4b51371573e48b932c14f165ba5e735
Sudhaperumalsamy <sudha.perumalsamy@yahoo.com>
2020-11-27 18:31:13 +0530

    seventh commit

---
+++ Projects/CalculatorDemo/src/sudha/mycodingjourney/Calculator.java
@@ -0,0 +1,40 @@
+package sudha.mycodingjourney;
+
+
+public class Calculator {
+	
+	public void add(int a,int b)
+	
+	{
+		int c;
+		c=a+b;
+		System.out.println("Addition :" +c);
+	}
+	public void subtract(int a,int b)
+	{
+		int c;
+		c=a-b;
+		System.out.println("Subtraction :" +c);
+		
+	}
+	public void multipy(int a,int b)
+	{
+		int c;
+		c=a*b;
+		System.out.println("Multiplier :" +c );
+	}
+	public void divider(int a,int b)
+	{
+		int c;
+		c=a/b;
+		System.out.println("divider :"+c);
+	}
+	public void modulus(int a,int b)
+	{
+		int c;
+		c=a%b;
+		System.out.println("modulus :"+c);
+	}
+	
+}
+
---
+++ Projects/Helloworld/src/sudha/mycodingjourney/Helloworld.java
@@ -0,0 +1,11 @@
+package sudha.mycodingjourney;
+
+public class Helloworld {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World,my java");
+	
+	}
+
+}
